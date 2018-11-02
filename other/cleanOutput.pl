e41ace2ad93a03ec9fb8011205bac7f91170ed60
henryst57 <henrys@vcu.edu>
2018-11-02 10:26:50 -0400

    updated cleanoutput.pl to work with new output

--- other/cleanOutput.pl
+++ other/cleanOutput.pl
@@ -7,21 +7,9 @@ my $inFile = 'trainingOut_2000BatchSize_withWeights';
 my $outFile = "cleaned_$inFile";
 #&_cleanOutput($inFile, $outFile);
 
-$inFile = 'nohup_200_learningRate_0.5';
+$inFile = 'output_replicateOld';
 $outFile = $inFile;
-$outFile =~ s/nohup/trainingOut_batch/;
-$outFile .= '_cleaned';
-&_cleanOutput($inFile, $outFile);
-
-$inFile = 'nohup_200_learningRate_0.7';
-$outFile = $inFile;
-$outFile =~ s/nohup/trainingOut_batch/;
-$outFile .= '_cleaned';
-&_cleanOutput($inFile, $outFile);
-
-$inFile = 'nohup_200_learningRate_0.9';
-$outFile = $inFile;
-$outFile =~ s/nohup/trainingOut_batch/;
+#$outFile =~ s/nohup/trainingOut_batch/;
 $outFile .= '_cleaned';
 &_cleanOutput($inFile, $outFile);
 
@@ -43,19 +31,24 @@ sub _cleanOutput {
     print OUT "Batch Number\tETA\tLoss\tAccuracy\tPrecision\tRecall\tMatthews Correlation\n";
 
     #process each line of the file
+    my $batchNumber = 1;
     while (my $line = <IN>) {
 
 	#only read in lines reporting the progres
-	if ($line =~ /(\d+)\/\d+ .+ - ETA: (\d+:\d{2}) - loss: (\d+\.\d+e?[+-]?\d*) - acc: (\d+\.\d+e?[+-]?\d*) - precision: (\d+\.\d+e?[+-]?\d*) - recall: (\d+\.\d+e?[+-]?\d*) - matthews_correlation: (-?\d+\.\d+e?[+-]?\d*)/) {
+	if ($line =~ /(\d+)\/\d+ .+ - ETA: (\d+:\d{2}) - loss: (\d+\.\d+e?[+-]?\d*) - acc: (\d+\.\d+e?[+-]?\d*) - Precision: (\d+\.\d+e?[+-]?\d*) - Recall: (\d+\.\d+e?[+-]?\d*) - Matthews_Correlation: (-?\d+\.\d+e?[+-]?\d*)/) {
 	    
 	    #grab the values
-	    my $batchNumber = $1;
+	    #my $batchNumber = $1;
 	    my $eta = $2;
 	    my $loss = $3;
 	    my $acc = $4;
 	    my $precision = $5;
 	    my $recall = $6;
 	    my $mcc = $7;
+
+	    #keep your own count of batch number
+	    # because it resets at the end of each epoch
+	    $batchNumber++;
 
 	    #output the values in a tab seperated format
 	    print OUT "$batchNumber\t$eta\t$loss\t$acc\t$precision\t$recall\t$mcc\n";
