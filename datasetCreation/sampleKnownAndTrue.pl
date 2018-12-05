af04613b128f0d9549ccf8e7fd2ed4abb837f4fc
henryst@vcu.edu <henryst@vc.edu>
2018-12-05 18:26:02 -0500

    added debugging config files and a method to sample known and true matrices based on the vocabulary size

---
+++ datasetCreation/sampleKnownAndTrue.pl
@@ -0,0 +1,67 @@
+#Program to reduce the known and true datasets based on randomly
+# selecting n terms from the vocabulary
+use strict;
+use warnings;
+use lib '../';
+use utilities;
+
+#User Input
+my $knownFile = '../../data/testTrain/known';
+my $trueFile = '../../data/testTrain/true';
+my $reducedVocabSize = 10000;
+
+#Auto-Generated Output FileNames
+my $knownOut = "../../data/testTrain/known_$reducedVocabSize";
+my $trueOut = "../../data/testTrain/true_$reducedVocabSize";
+
+
+
+################################################
+# Begin Code
+################################################
+
+
+#read the matrix and vocab
+my $knownMatrix = &utilities::readSemMedFile($knownFile);
+my ($vocabularyRef, $relationTypesRef) = &utilities::getVocabularyAndPredicateTypes($knownMatrix);
+
+#randomly?? select $vocabSize terms
+my %sampledVocab = ();
+foreach my $cui (keys %{$vocabularyRef}) {
+    $sampledVocab{$cui} = 1;
+    if (scalar keys %sampledVocab >= $reducedVocabSize) {
+	last;
+    }
+}
+
+#generate the reduced known set
+my %reducedKnownMatrix = ();
+foreach my $triplet (keys %{$knownMatrix}) {
+    my ($subject, $predicate, $object) = split(',',$triplet);
+    if (defined $sampledVocab{$subject} && defined $sampledVocab{$object}) {
+	$reducedKnownMatrix{$triplet} = 1;
+    }
+}
+
+#generate the reduced true set
+my $trueMatrix = &utilities::readSemMedFile($trueFile);
+my %reducedTrueMatrix = ();
+foreach my $triplet (keys %{$trueMatrix}) {
+    my ($subject, $predicate, $object) = split(',',$triplet);
+    if (defined $sampledVocab{$subject} && $sampledVocab{$object}) {
+	$reducedTrueMatrix{$triplet} = 1;
+    }
+}
+
+print "size of reducedKnown: ".(scalar keys %reducedKnownMatrix)."\n";
+print "size of reducedTrue: ".(scalar keys %reducedTrueMatrix)."\n";
+print "size of sampledVocab: ".(scalar keys %sampledVocab)."\n";
+
+#output the reduced known and reduced true
+&utilities::outputDataset(\%reducedKnownMatrix, $knownOut);
+&utilities::outputDataset(\%reducedTrueMatrix, $trueOut);
+
+#Done!
+print "Done!\n";
+
+
